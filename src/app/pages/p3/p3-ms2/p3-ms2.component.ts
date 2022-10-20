956d3857f5d1bb96fce9364076f19c5bc3f8c927
Tim Ho <github.timho@gmail.com>
2022-10-19 18:16:14 -0700

    Refactor: add p3-ms2

--- src/app/pages/p3/p3-ms2/p3-ms2.component.ts
+++ src/app/pages/p3/p3-ms2/p3-ms2.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-p3-ms2',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./p3-ms2.component.scss']
 })
 export class P3Ms2Component implements OnInit {
+  public data: number = 0
+  // public type: string = '';
+  public isHiddenPk: boolean = false
+  public isHiddenMov: boolean = true
+  public pkName: string = '';
+  public pkId: number = 0;
+  public pkImg: string = '';
+  @Input() type: string = ''
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
   }
 
+
+  public isHiddenPokemon() {
+    this.isHiddenPk = false
+    this.isHiddenMov = true
+  }
+  public isHiddenMoves() {
+    this.isHiddenMov = false
+    this.isHiddenPk = true
+  }
 }
